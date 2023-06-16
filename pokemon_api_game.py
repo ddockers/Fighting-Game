d763bf3617a6866fdff123edfb8d9a4b09f37a6d
Ash Frankland <afrankland@spartaglobal.com>
2023-06-16 16:51:45 +0100

    added player and cpu pokemon

--- pokemon_api_game.py
+++ pokemon_api_game.py
@@ -1,10 +1,18 @@
 import requests
 from random import randint
 
-rand_num = randint(1, 151)
+rand_num_1 = randint(1, 151)
+rand_num_2 = randint(1, 150)
 
-pokemon_req = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(rand_num))
+pokemon_req_player = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(rand_num_1))
+pokemon_req_cpu = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(rand_num_2))
 
-our_poke = pokemon_req.json()
+player_poke = pokemon_req_player.json()
+cpu_poke = pokemon_req_cpu.json()
 
-print(our_poke['name'])
+# print(our_poke.keys())
+print(player_poke['name'])
+print(player_poke['base_experience'])
+
+print(cpu_poke['name'])
+print(cpu_poke['base_experience'])
